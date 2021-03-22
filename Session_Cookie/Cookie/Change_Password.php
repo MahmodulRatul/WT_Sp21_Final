5486dfbc667db04fb68555544dda82d499dd30ed
Md Mahmodul Kabir Ratul <62782054+MahmodulRatul@users.noreply.github.com>
2021-03-22 03:39:00 -0600

    Session-Cookie task done.

---
+++ Session_Cookie/Cookie/Change_Password.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php
+	session_start();
+	if(!isset($_COOKIE["user"])  && !isset($_COOKIE["pass"]))
+		header ("Location: Log_in.php");
+	else
+	echo $_COOKIE["user"]." You can change password here. Old pass is: ".$_COOKIE["pass"];
+?>
+</body>
+</html>
---
+++ Session_Cookie/Session/Change_Password.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php
+	session_start();
+	if(!isset($_SESSION["user"])  && !isset($_SESSION["pass"]))
+		header ("Location: Log_in.php");
+	else
+	echo $_SESSION["user"]." You can change password here. Old pass is: ".$_SESSION["pass"];
+?>
+</body>
+</html>
